53a88b89c9682d3b9fe96a5d689be8b10af9d831
Suhag Al Amin <developersuhag@gmail.com>
2021-12-17 10:46:02 +0600

    redux persist

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import "./index.css";
-import { store } from "./redux/store";
+import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bookSlice from "./slices/bookSlice";
 
-export const store = configureStore({
-  reducer: {
-    books: bookSlice,
-  },
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "books",
+  storage,
+};
+
+const reducers = combineReducers({ books: bookSlice });
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+// export const store = configureStore({
+//   reducer: {
+//     books: bookSlice,
+//   },
+// });
+
+export default configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
